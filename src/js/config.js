7922dd3b0e511aeeb21d8c4598eb2ac1777b1023
JEFF HENRY <henry_jf@hotmail.com>
2016-05-22 08:47:57 -0400

    added templates and defined them in config file

--- src/js/config.js
+++ src/js/config.js
@@ -2,6 +2,24 @@ function config($stateProvider, $urlRouterProvider) {
 
   $urlRouterProvider.otherwise('/');
 
+  //define app states
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'templates/home.tpl.html',
+      controller: 'HomeController'
+    })
+    .state('add', {
+      url: '/add',
+      templateUrl: 'templates/add.tpl.html',
+      controller: 'AddController'
+    })
+    .state('details', {
+      url: '/details',
+      templateUrl: 'templates/details.tpl.html',
+      controller: 'DetailsController'
+    })
 }
 
 config.$inject = ['$stateProvider', '$urlRouterProvider'];
