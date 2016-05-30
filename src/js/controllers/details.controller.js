18f5c5706e5e06c0a50acc4480ed35de23bcfd34
JEFF HENRY <henry_jf@hotmail.com>
2016-05-30 08:49:19 -0400

    adding functions to details controller

--- src/js/controllers/details.controller.js
+++ src/js/controllers/details.controller.js
@@ -1,10 +1,25 @@
-function DetailsController ($http, URL, $stateParams) {
+function DetailsController ($http, URL, $stateParams, $state) {
 
-  let vm = this
+  let vm = this;
 
-console.log($stateParams);
+  vm.deleteImage = deleteImage;
+
+  init();
+
+  function init() {
+    $http.get(URL + $stateParams.imageId).then( (res)=> {
+      vm.detailsImage = res.data;
+    });
+  }
+
+  function deleteImage (imageId) {
+    $http.delete(URL + imageId).then( (res) =>{
+      $state.go('home')
+    })
+  }
+// console.log($stateParams);
 
 }
 
-DetailsController.$inject = ['$http', 'URL', '$stateParams'];
+DetailsController.$inject = ['$http', 'URL', '$stateParams', '$state'];
 export { DetailsController };
